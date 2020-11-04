1ab2cc8b11e20f0f2e5a726f6cae7e11315a8909
Garry Eves <garry@eves.id.au>
2020-11-05 07:52:09 +0800

    Initial Commit

---
+++ Shared/decodeMaintenanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  decodeMaintenanceApp.swift
+//  Shared
+//
+//  Created by Garry Eves on 5/11/20.
+//
+
+import SwiftUI
+
+@main
+struct decodeMaintenanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
